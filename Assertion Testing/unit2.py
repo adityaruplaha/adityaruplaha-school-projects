7840df236f1ad14640a0ff3427cf6b3088b2fae7
Adityarup Laha <30696515+adityaruplaha@users.noreply.github.com>
2020-02-24 22:51:26 +0530

    AssertionTesting: Test certain conjectures. (#5)
    * unit0.py: [(-a) // b] == [a // (-b)]
    * unit1.py: [(-a) % b] == -[a % (-b)]
    * unit2.py: [(-a) % b] + [a % b] == b
    
    All yielded True.

---
+++ Assertion Testing/unit2.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+from random import randint
+
+def test(a : int, b : int) -> bool:
+    p = (-a) % b
+    q = a % b
+    S = (p + q == b)
+    print("[-{a} % {b}] + [{a} % {b}] == {b} : {S}".format(a=a, b=b, S=S))
+    return S
+
+I = True
+
+N = 10000
+
+for _ in range(N):
+    A = randint(0, 1000000)
+    B = randint(1, 1000000)
+    I &= test(A, B)
+
+print("Assertion: [(-a) % b] + [a % b] == b.")
+print("Unit testing {n} elements: {i}".format(n=N, i=I))
